87c1085e0a7275285e15f44c49371508cfebebb7
jonaswrg <jonas.woergau@students.fhnw.ch>
2022-09-30 14:32:25 +0200

    Abgabe Uebungen

---
+++ Uebung02/Aufgabe1.js
@@ -0,0 +1,5 @@
+var L = []
+for (var i = 1;i < 100;i = i+2){
+    L.push(i)
+    console.log(L);
+}
---
+++ Uebung02/Aufgabe2.js
@@ -0,0 +1,7 @@
+var zahl = Math.floor(6*Math.random())+1;
+
+function wuerfeln(){
+    return(zahl)
+}
+
+console.log(wuerfeln(zahl))
