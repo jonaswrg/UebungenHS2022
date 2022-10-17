cd248c14e3474829839420cf19a38d0a414d4235
jonaswrg <jonas.woergau@students.fhnw.ch>
2022-10-17 16:29:11 +0200

    Abgabe Uebung04

---
+++ Uebung04/projekt/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Timer from  "./Timer";
+
+
+function App() {
+
+
+    return(<>
+            <Timer countdown="50" />
+        </>
+        );
+}
+
+export default App;
